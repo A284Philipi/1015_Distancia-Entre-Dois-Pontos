901caa239bfeb4a6698ac63c363683922d68d2b0
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-12 11:30:19 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    double x1, y1, x2, y2, distancia;
+    cin >> x1;
+    cin >> y1;
+    cin >> x2;
+    cin >> y2;
+    distancia = sqrt((pow((x2-x1),2)+pow((y2-y1),2)));
+    cout.precision(4);
+    cout << fixed << distancia <<endl;
+    return 0;
+}
